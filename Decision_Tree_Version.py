46c9a394a3b2efb8159d699af9bdc5d526effa8a
GeorgeAttaloglou <georgeeattaloglou@gmail.com>
2024-11-16 14:37:56 +0200

    Decision tree version is effectively finished

--- Decision_Tree_Version.py
+++ Decision_Tree_Version.py
@@ -1,25 +1,15 @@
-from ucimlrepo import fetch_ucirepo
-import numpy as np # Εισάγει τη βιβλιοθήκη NumPy, η οποία παρέχει υποστήριξη για πίνακες (arrays) και αριθμητικούς υπολογισμούς.
-import seaborn as sns
-import scipy as sp # Εισάγει τη βιβλιοθήκη SciPy, η οποία χρησιμοποιείται για επιστημονικούς υπολογισμούς.
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn import tree # Εισάγει το υποσύνολο tree από τη βιβλιοθήκη scikit-learn, το οποίο περιλαμβάνει υλοποιήσεις αλγορίθμων δέντρου απόφασης.
-from sklearn.utils import resample # Εισάγει τη συνάρτηση resample, η οποία χρησιμοποιείται για την επαναδειγματοληψία (resampling) δεδομένων.
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.ensemble import RandomForestClassifier # Εισάγει τον ταξινομητή τυχαίου δάσους RandomForestClassifier, ο οποίος είναι ένας αλγόριθμος που χρησιμοποιεί πολλά δέντρα απόφασης για να βελτιώσει την ακρίβεια των προβλέψεων.
-import matplotlib as mpl # Εισάγει τη βιβλιοθήκη matplotlib, που χρησιμοποιείται για τη δημιουργία γραφημάτων και οπτικοποιήσεων.
-import matplotlib.cm as cm # Εισάγει το υποσύνολο cm της βιβλιοθήκης matplotlib, που παρέχει έτοιμες παλέτες χρωμάτων (colormaps) για τη δημιουργία γραφημάτων.
-import matplotlib.pyplot as plt
-import pandas as pd
-pd.set_option('display.width', 500)
-pd.set_option('display.max_columns', 100)
-pd.set_option('display.notebook_repr_html', True)
-from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from ucimlrepo import fetch_ucirepo  # Εισαγωγή της βιβλιοθήκης για λήψη δεδομένων από το UCI ML Repository
+import numpy as np  # Εισαγωγή της βιβλιοθήκης numpy για αριθμητικές πράξεις
+from sklearn.model_selection import train_test_split, cross_val_score  # Εισαγωγή συναρτήσεων για διαχωρισμό δεδομένων και διασταυρούμενη επικύρωση
+from sklearn.tree import DecisionTreeClassifier  # Εισαγωγή του ταξινομητή δέντρου απόφασης
+import matplotlib.pyplot as plt  # Εισαγωγή της βιβλιοθήκης matplotlib για γραφήματα
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay  # Εισαγωγή συναρτήσεων για υπολογισμό ακρίβειας και εμφάνιση πίνακα σύγχυσης
 
 # Παίρνουμε το dataset
 phishing_websites = fetch_ucirepo(id=327) 
 
-# Αποθηκεύουμε τα χαρακτηριστικά και τις ετικέτες του dataset σε μεταβλητές
+# Αποθηκεύουμε τα χαρακτηριστικά και τις ετικέτες του dataset σε pandas dataframe
+#Με την dropna() αφαιρούμε τις ελλιπείς τιμές
 X = phishing_websites.data.features.dropna()
 y = phishing_websites.data.targets.dropna()
 
@@ -34,15 +24,14 @@ plt.show()
 # Διαχωρίζουμε τα δεδομένα σε training και testing datasets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-#TODO: Play around with max_leaf_nodes and keep track of the results
 # Δημιουργούμε έναν ταξινομητή δέντρου απόφασης
-DecisionTree = DecisionTreeClassifier(random_state=1)
+DecisionTree = DecisionTreeClassifier(random_state=1, max_leaf_nodes=15)
 
-# Υπολογίζουμε τα σκορ διασταυρούμενης επικύρωσης για τον ταξινομητή DecisionTree
+# Υπολογίζουμε τα σκορ διασταυρούμενης επικύρωσης για το DecisionTree
 Cross_Validation_Score = cross_val_score(DecisionTree, X_train, y_train, cv=10) 
 print("Cross Validation Score: ", np.mean(Cross_Validation_Score))
 
-# Εκπαιδεύουμε τον ταξινομητή DecisionTree στο training dataset
+# Εκπαιδεύουμε τον ταξινομητή DecisionTree
 DecisionTree.fit(X_train, y_train)
 
 # Κάνουμε προβλέψεις στο testing dataset
@@ -52,12 +41,7 @@ y_pred = DecisionTree.predict(X_test)
 Accuracy = accuracy_score(y_test, y_pred)
 Confusion_matrix = confusion_matrix(y_test, y_pred)
 print("Accuracy: ", Accuracy)
-#print("Confusion Matrix:\n", Confusion_matrix)
 
 disp = ConfusionMatrixDisplay(confusion_matrix = Confusion_matrix)
 disp.plot(cmap=plt.cm.Blues)
 plt.show()
-
-
-#plt.figure(figsize=(13, 8)) # Ορίζουμε μέγεθος γραφήματος
-#tree_vis = plot_tree(DecisionTree, filled=True) # Δημιουργούμε γράφημα δέντρου με τα χρώματα να υποδεικνύουν την κατανομή των κλάσεων
